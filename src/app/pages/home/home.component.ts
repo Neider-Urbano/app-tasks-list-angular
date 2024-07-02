33b04d1f5062df1bff59d5c11f4f8554b2f0006d
neider urbano <jjuly8julianur@gmail.com>
2024-07-02 12:47:37 -0500

    initial project config

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  tasks = signal([
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear componente',
+    'Crear  servicio',
+  ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
+}
